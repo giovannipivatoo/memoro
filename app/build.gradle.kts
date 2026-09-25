d99ae8ec7395519078e0224edc4822d290dbb1de
giopiz <giopiz@Mac.homenet.telecomitalia.it>
2026-09-25 04:07:17 +0200

    build: pin Gradle wrapper and Android Zstandard support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.github.luben:zstd-jni:1.5.7-3@aar")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
